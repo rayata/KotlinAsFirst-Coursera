033e3b0eff49fe649ff1b381457fa7cf9f2ebdb7
Lipatova <Raisa.Lipatova@billing.ru>
2019-02-12 19:16:36 +0300

    Exercises from Lesson 1 full. Part from Lesson 2

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -58,7 +58,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*60*60 + minutes * 60 + seconds
 
 /**
  * Тривиальная
@@ -67,7 +67,13 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+    val vershokToSm = 4.445
+    val arshinToVershok = 16
+    val sageneToVershok = 48
+    val result = ((sagenes*sageneToVershok + arshins*arshinToVershok + vershoks ) * vershokToSm) / 100;
+    return result;
+}
 
 /**
  * Тривиальная
@@ -75,15 +81,21 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
-
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    val p = 3.141592653589793238462643
+    val oneDegreeInRadian = p/180;
+    val oneMinInRadian = oneDegreeInRadian/60;
+    val oneSecInRadian = oneMinInRadian/60;
+    val result = deg*oneDegreeInRadian + min * oneMinInRadian + sec * oneSecInRadian
+    return result
+}
 /**
  * Тривиальная
  *
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1-x2)+ sqr(y1-y2))
 
 /**
  * Простая
@@ -91,7 +103,13 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    val str = number.toString()
+    val index = str.lastIndex - 2
+    val digitsArray = str.map { it.toString().toInt() }
+    val thirdDigit = digitsArray[index]
+    return thirdDigit
+}
 
 /**
  * Простая
@@ -100,16 +118,21 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val departureTimeInSeconds = seconds(hoursDepart, minutesDepart, 0)
+    val arrivalTimeInSeconds = seconds(hoursArrive, minutesArrive, 0)
+    val travelTimeInMinutes =  (arrivalTimeInSeconds - departureTimeInSeconds)/60
+    return travelTimeInMinutes
+}
 
 /**
  * Простая
  *
  * Человек положил в банк сумму в s рублей под p% годовых (проценты начисляются в конце года).
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
- * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
+ * Например, 100 рублей под 10% годовых превратятся в 133.1 рубляSUM = X * (1 + %)n
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double = (initial * (1 + percent.toDouble()/100).pow(3))
 
 /**
  * Простая
@@ -117,4 +140,13 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val str = number.toString()
+    val digitsArray = str.map { it.toString().toInt() }
+    var res = ""
+    for (i in digitsArray.lastIndex downTo 0 step 1) {
+        res = res + digitsArray[i]
+    }
+    val result = res.toInt()
+ return result
+}
--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -62,7 +62,33 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    var results = ""
+    val arrayGod = arrayOf(1, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121, 131, 141, 151, 161, 171, 181, 191)
+    val arrayGoda = intArrayOf(2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54, 62, 63, 64, 72, 73, 74,
+            82, 83, 84, 92, 93, 94, 102, 103, 104, 122, 123, 124, 132, 133, 134, 142, 143, 144, 152, 153, 154, 162, 163, 164, 172,
+            173, 174, 182, 183, 184, 192, 193, 194)
+    val arrayLet = (5..20).toList() + (25..30).toList() + (35..40).toList() + (45..50).toList() + (55..60).toList()+ (65..70).toList()+
+            (75..80).toList() + (85..90).toList()+ (105..119).toList() + (125..130).toList() + (135..140).toList()+ (145..150).toList() +
+            (155..160).toList() + (165..170).toList() + (175..180).toList()+ (185..190).toList()+ (195..200).toList()
+    if (age < 0 || age > 200){
+        if(age < 0) {
+            results = "Age is less than 0. Enter correct age"
+        }
+        else {
+            results = "Age is more than 200. Enter correct age"
+        }
+    }
+    else{
+        when(age){
+            in arrayGod -> results  = "$age год"
+            in arrayGoda -> results = "$age года"
+            in arrayLet -> results = "$age лет"
+            else -> results = "Something went wrong"
+        }
+    }
+    return results
+}
 
 /**
  * Простая
@@ -73,7 +99,24 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    var result = 0.0
+    val halfWay = (t1*v1 + t2*v2 + t3*v3)/2
+   // val firstDiv = halfWay/v1
+    //val firstDivRemain = halfWay%v1
+    if(halfWay%v1 <= 0) {
+        result = halfWay/v1
+    }
+    else {
+        if (halfWay%v1%v2 > 0){
+            result = halfWay/v1 + halfWay/v1/v2 + halfWay/v1/v2/v3
+        }
+        else{
+            result = (halfWay - halfWay%v1)/v1 + halfWay%v1/v2
+        }
+    }
+    return result
+}
 
 /**
  * Простая
@@ -120,4 +163,39 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    var result = 0
+    if (b >= a && d >= c) {
+
+        if (a in c..d){
+            if(b in c..d){
+                result = b - a;
+            }
+            else{
+                result=d - a
+            }
+        }
+        else{
+            if(b in c..d){
+                result = b - c;
+            }
+            else{
+                if(c in a..b){
+                    if(d in a..b){
+                        result = d - c
+                    }
+                    else{
+                        result = b - c
+                    }
+                }
+                else{
+                    result=-1
+                }
+
+            }
+        }
+
+
+    }
+    return result
+}
